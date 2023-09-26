f1385fd1004b02034eb16ca21b27236b43244c67
Ylarod <me@ylarod.cn>
2023-09-26 15:59:42 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 for i in {33..21}; do
     echo "start aosp_opengrok_sdk$i"
-    docker-compose up --profile -d aosp_opengrok_sdk$i
+    docker-compose --profile init up -d aosp_opengrok_sdk$i
     sleep 3000
 done
+docker-compose --profile init down
